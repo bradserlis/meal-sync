0a085cffa3c12e2f3666863a614242471c41cd81
bradserlis <bradserlis@yahoo.com>
2020-07-06 15:11:37 -0700

    WIP

--- src/Navigation/Router.tsx
+++ src/Navigation/Router.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import Landing from '../Landing/Landing.tsx';
+import Landing from '../Landing/Landing';
+import SplashScreen from './SplashScreen';
+import SignUpScreen from './SignUpScreen';
+import SignInScreen from './SignInScreen';
+import Home from '../Profile/containers/Home';
 
 const DetailsScreen = () => {
-  return(
+  return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
     </View>
@@ -15,18 +19,39 @@ const DetailsScreen = () => {
 
 const Stack = createStackNavigator();
 
-const Router = () => 
+const Router = () => {
+
+  if (state.isLoading) {
+    return <SplashScreen />
+  }
+
+  return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Landing">
-        <Stack.Screen
-          name="Landing" 
-          component={Landing}
-          options={{
-            headerShown: false
-          }}
-           />
-        <Stack.Screen name="Details" component={DetailsScreen} />
+      <Stack.Navigator>
+        {state.userToken == null ?
+          (
+            <>
+              <Stack.Screen
+                name="SignIn"
+                component={SignInScreen}
+                options={{ title: 'Sign In' }}
+              />
+              <Stack.Screen
+                name="SignUp"
+                component={SignUpScreen}
+                options={{ title: 'Sign Up' }}
+              />
+            </>
+          ) : (
+            <Stack.Screen
+              name="Home"
+              component={Home}
+            />
+          )
+        }
       </Stack.Navigator>
     </NavigationContainer>
+  )
+}
 
 export default Router;
