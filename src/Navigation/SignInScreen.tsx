aa08fc6f17f7d10e62409fca819b0952c3fc4420
bradserlis <bradserlis@yahoo.com>
2020-07-14 16:50:22 -0700

    Add signin error handling logic

--- src/Navigation/SignInScreen.tsx
+++ src/Navigation/SignInScreen.tsx
@@ -9,10 +9,34 @@ import { globalStyles } from '../globalStyles'
 const SignInScreen = ({navigation}) => {
 
   let handleSignIn = () => {
-    firebase.auth().signInWithEmailAndPassword(email, password).then((response) => {
-      alert('signed in properly!');
-      navigation.navigate('Home')
-    })
+    try {
+      firebase.auth().signInWithEmailAndPassword(email, password).then(() => {
+        alert('signed in properly!');
+        navigation.navigate('Home')
+      }).catch(error => {
+        switch(error.code){
+          case 'auth/user-not-found':
+            alert('No user found. Please check your details and try again')
+            break;
+          case 'auth/wrong-password':
+            alert('Your password is incorrect')
+            break;
+          case 'auth/invalid-email':
+            alert('The email entered is not formatted correctly - "me@something.com"')
+            break;
+          case 'auth/user-disabled':
+            alert('Your account is currently disabled')
+            break;
+          default:
+            alert('Something has gone wrong. Please try again.')
+            break;
+        }
+      }) 
+    }
+    catch(e) {
+      alert(e)
+      console.log(e)
+    }
   }
 
   const [email, setEmail] = useState('');
