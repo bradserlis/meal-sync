0f219eb2d0dc39262dee135181b4a0def2efa683
bradserlis <bradserlis@yahoo.com>
2020-07-21 15:22:05 -0700

    Adds dialog of formatted connectionId, prompt to connect, cancel functionality

--- src/Connections/containers/AddConnection.tsx
+++ src/Connections/containers/AddConnection.tsx
@@ -1,25 +1,48 @@
 import React, { useState } from 'react';
 import { View, KeyboardAvoidingView, Platform } from 'react-native';
-import { Paragraph, Title, Headline, TextInput, Button } from 'react-native-paper';
+import { Paragraph, Title, Headline, TextInput, Button, Dialog, Portal } from 'react-native-paper';
 
 import { globalStyles } from '../../globalStyles'
 
-const AddConnection = () => {
+const AddConnection = (props) => {
 
   const [searchText, setSearchText] = useState('')
+  const [dialogVisibility, setDialogVisibility] = useState(false)
+  const [dialogMessage, setDialogMessage] = useState('')
 
-  const onSubmit = () => {
-    let formatter = searchText.slice(0, 4) + '-' + searchText.slice(4)
-    setSearchText(formatter)
+  let hideDialog = () => setDialogVisibility(false);
+
+  const onSubmit = async () => {
+    if(searchText.length === 8){
+      let formatter = searchText.slice(0, 4) + '-' + searchText.slice(4)
+      await setSearchText(formatter)
+      props.createConnection(formatter)
+      props.toggleShowDialog()
+      setSearchText('')
+    } else{
+      setDialogMessage('Invalid ConnectionID length - 8 Characters Required')
+      setDialogVisibility(!dialogVisibility)
+    }
   }
 
   return(
-      <KeyboardAvoidingView
-        behavior={Platform.OS == "ios" ? "padding" : "height"}
-        style={{display: 'flex', flex: .5, flexDirection: 'row'}}
-        keyboardVerticalOffset={150}  
-      >
-        <View style={{display: 'flex', flex: 1, justifyContent: 'center', alignSelf: 'flex-end'}}>
+    <KeyboardAvoidingView
+      behavior={Platform.OS == "ios" ? "padding" : "height"}
+      style={{display: 'flex', flex: .5, flexDirection: 'row'}}
+      keyboardVerticalOffset={150}  
+    >        
+    <Portal>
+        <Dialog visible={dialogVisibility} onDismiss={hideDialog}>
+          <Dialog.Title>Oops...</Dialog.Title>
+          <Dialog.Content>
+            <Paragraph>{dialogMessage}</Paragraph>
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button onPress={hideDialog}>Done</Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
+      <View style={{display: 'flex', flex: 1, justifyContent: 'center', alignSelf: 'flex-end'}}>
         <Title>Add Connection</Title>
         <TextInput 
         label='Connection ID'
@@ -37,8 +60,8 @@ const AddConnection = () => {
         >
         Add Connection
         </Button>
-        </View>
-      </KeyboardAvoidingView>
+      </View>
+    </KeyboardAvoidingView>
   )
 }
 
--- src/Connections/containers/Connections.tsx
+++ src/Connections/containers/Connections.tsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { View, FlatList, ScrollView } from 'react-native'
-import { Headline, Paragraph, Button, Title,  } from 'react-native-paper';
+import { Headline, Paragraph, Button, Title, Dialog, Portal, Text } from 'react-native-paper';
+import * as firebase from 'firebase'
 
-import { globalStyles } from '../../globalStyles';
+import { globalStyles, dimensions } from '../../globalStyles';
 import YourConnections from './YourConnections';
 import AddConnection from './AddConnection'
 
@@ -32,20 +33,50 @@ const dummyData = [
 const Connections = ({ navigation }) => {
 
   const [connectionCards, setConnectionCards] = useState([])
+  const [showDialog, setShowDialog] = useState(false)
+  const [connectionSearchResults, setConnectionSearchResults] = useState('')
+  const [userId] = useState(firebase.auth().currentUser.uid)
+
+  let toggleShowDialog = () => {
+    setShowDialog(!showDialog)
+  }
+
+  let createConnection = (user) => {
+    setConnectionSearchResults(user)
+    console.log('sanity check - user', connectionSearchResults)
+  }
+
+  let addConnectionId = (user) => {
+    console.log('adding', connectionSearchResults, 'to', userId)
+    toggleShowDialog()
+  }
 
   useEffect(() => {
     setConnectionCards(dummyData)
   }, [connectionCards])
 
   return(
-    <View style={globalStyles.container}>
-      <View style={globalStyles.dividerDiv}>
-        <Headline> Connections </Headline>
+      <View style={globalStyles.container}>
+        <View style={globalStyles.dividerDiv}>
+          <Headline> Connections </Headline>
+        </View>
+        <Portal>
+        <Dialog visible={showDialog} onDismiss={toggleShowDialog}>
+          <Dialog.Title>Confirm Connection</Dialog.Title>
+          <Dialog.Content>
+            <Paragraph>You are trying to add user: <Paragraph style={{fontWeight: 'bold'}}>{connectionSearchResults}</Paragraph></Paragraph>
+            <Paragraph>To continue, press Connect below</Paragraph>
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button style={{margin: 5}} mode={'outlined'} onPress={addConnectionId}>Connect</Button>
+            <Button style={{margin: 5}} mode={'outlined'} onPress={toggleShowDialog}>Cancel</Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
+        <YourConnections connections={connectionCards}/>
+        <AddConnection toggleShowDialog={toggleShowDialog} createConnection={createConnection} />
       </View>
-      <YourConnections connections={connectionCards}/>
-      <AddConnection />
-    </View>
-    )
+  )
 }
 
 export default Connections;
