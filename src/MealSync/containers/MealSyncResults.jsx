4265d69db670890f277d6ed3c5604f08e8a426b3
bradserlis <bradserlis@yahoo.com>
2020-09-09 15:09:27 -0700

    Adds setSyncedResults function, which compares all user results and creates a final results object containing only restaurants that all users agreed to

--- src/MealSync/containers/MealSyncResults.jsx
+++ src/MealSync/containers/MealSyncResults.jsx
@@ -26,30 +26,67 @@ const MealSyncResults = ({ navigation }) => {
             Object.assign(dataObj, item.val())
         })
         setMealSyncObject(dataObj);
+        setSyncedResults();
+    }
+
+    const setSyncedResults = () => {
+        let resultsObj = Object.entries(mealSyncObject.results);
+        console.log(resultsObj);
+        let syncedResultsMap = {}
+        for (let i = 0; i < Object.keys(mealSyncObject.results).length; i++) {
+            console.log('in loop', resultsObj[i][1])
+            for (let key in resultsObj[i][1]) {
+                console.log('nested loop...show me the value!', key, `${resultsObj[i][1][key]}`)
+                // set key on Map if value is 1 for all "i's"
+                if (resultsObj[i][1][key] === 1) {
+                    if (syncedResultsMap[key] !== 0) {
+                        syncedResultsMap[key] = 1;
+                    }
+                } else {
+                    syncedResultsMap[key] = 0;
+                }
+            }
+        }
+        console.log('how did syncedResultsMap end up', syncedResultsMap);
+        for (let key in syncedResultsMap){
+            if(syncedResultsMap[key] === 0){
+                delete syncedResultsMap[key]
+            }
+        }
+        console.log('updated syncedResultsMap?', syncedResultsMap);
+        setResults(syncedResultsMap)
     }
 
     const getMealSyncQuery = () => {
         return firebase
-        .database()
-        .ref('mealsync-groups')
-        .orderByChild('users/'+currentUserObject.displayName)
-        .equalTo(currentUserObject.connectionId)
-        .once('value')
+            .database()
+            .ref('mealsync-groups')
+            .orderByChild('users/' + currentUserObject.displayName)
+            .equalTo(currentUserObject.connectionId)
+            .once('value')
     }
 
     const refreshResults = () => {
-        if(verifyAllUsersResponded() === true){
+        if (verifyAllUsersResponded() === true) {
             return setIncompleteResults(false)
         } else {
             alert('Some users have not finished responding to your Meal Sync. Please try again.')
         }
     }
 
+    const resultsRenderer = () => {
+        console.log('resultsRenderer - sanity check - results', results)
+        for (let key in results){
+            console.log('resultsRenderer log', key)
+            return <Paragraph>{key}</Paragraph>
+        }
+    }
+
     const verifyAllUsersResponded = () => {
         //check if all users assigned to that mealsync object...
-        for (let [key, val] of Object.entries(mealSyncObject.users)){
+        for (let [key, val] of Object.entries(mealSyncObject.users)) {
             // have a corresponding results object
-            if(!mealSyncObject.results.hasOwnProperty(val)){
+            if (!mealSyncObject.results.hasOwnProperty(val)) {
                 return false
             }
         }
@@ -65,22 +102,25 @@ const MealSyncResults = ({ navigation }) => {
                 <Button
                     mode='contained'
                     dark={true}
-                    onPress={refreshResults}
+                    onPress={setSyncedResults/*refreshResults*/}
                 >
                     Check Results
                 </Button>
-                { 
+                {
                     incompleteResults ? (
-                        <View style={{display: 'flex', flex: 1, alignSelf: 'center', justifyContent: 'center'}}>
+                        <View style={{ display: 'flex', flex: 1, alignSelf: 'center', justifyContent: 'center' }}>
                             <Title>Click "Check Results" above to check if all users have submitted answers</Title>
                         </View>
                     ) :
-                    (
-                        <View>
-                            <Title>Results for Everyone</Title>
-                        </View>
-                    )
-                } 
+                        (
+                            <View>
+                                <Title>Results for Everyone</Title>
+                                {
+                                    resultsRenderer()
+                                }
+                            </View>
+                        )
+                }
                 <View style={{ display: 'flex', flex: 1, flexDirection: 'row' }}>
                     <View style={{ display: 'flex', flex: 1, justifyContent: 'center', alignSelf: 'flex-end' }}>
                         <Button
